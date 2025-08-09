01a5ceefff95d91e5d909c6111450420443232b6
DrZafarIqbal <drzaffariqbal@gmail.com>
2025-08-09 16:28:43 +0530

    Fix: remove undefined Dashboard route/link to unblock deploy

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
-
 import FarmsPage from './pages/FarmsPage';
-// (Import other pages later, but don’t render them twice.)
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <Navbar />
@@ -14,12 +12,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to="/farms" replace />} />
           <Route path="/farms" element={<FarmsPage />} />
-          {/* Add other single routes later, one per path */}
         </Routes>
       </div>
     </div>
   );
 }
-
-export default App;
-
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,7 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
     <nav style={{ padding: '0.75rem 1rem', borderBottom: '1px solid #eee' }}>
-      <Link to="/farms" style={{ marginRight: 16 }}>Farms</Link>
-      {/* Add other links later like: <Link to="/breeds">Breeds</Link> */}
+      <Link to="/farms">Farms</Link>
     </nav>
   );
 }
